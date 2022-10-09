7c1ac7e25ab671799e0967f0372e6e0228ab7d48
Andriy Nedvyha <andriy.nedvyha@ua-gis.com>
2022-10-09 19:57:32 +0300

    ADD: soap default encoder/decoder

--- src/main/java/com/example/webfluxtest/config/WebServiceConfig.java
+++ src/main/java/com/example/webfluxtest/config/WebServiceConfig.java
@@ -3,6 +3,8 @@ package com.example.webfluxtest.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.codec.xml.Jaxb2XmlDecoder;
+import org.springframework.http.codec.xml.Jaxb2XmlEncoder;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,8 +24,8 @@ public class WebServiceConfig {
     public WebClient webClient() {
         ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder().
                 codecs(clientCodecConfigurer -> {
-//                    clientCodecConfigurer.customCodecs().register(new Jaxb2SoapEncoder());
-//                    clientCodecConfigurer.customCodecs().register(new Jaxb2SoapDecoder());
+                    clientCodecConfigurer.customCodecs().register(new Jaxb2XmlEncoder());
+                    clientCodecConfigurer.customCodecs().register(new Jaxb2XmlDecoder());
                 }).build();
 
         return WebClient.builder()
